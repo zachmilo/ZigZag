69d4f5bc6fc99f395ef780a69758fe13e2fa58f9
zachmilo <zachmilo45@gmail.com>
2017-04-03 18:47:47 -0400

    shared js

--- main/assets/javascript/shared.js
+++ main/assets/javascript/shared.js
@@ -1,17 +1,28 @@
 "use strict"
-
+/*
+* Allow you to search spotify api.
+* This search can do artist, albums, and track
+* returns: the search results
+ */
 function spotifySearch(searchQuery,artist, albums,track) {
-    var artistType = artist || "";
-    var albumType = albums  || "";
-    var trackType = track   || "";
-    
+    var artistType = artist+"&"  || "";
+    var albumType  = albums+"&"  || "";
+    var trackType  = track+"&"   || "";
+
     var strippedQuery = _.replace(_.trim(searchQuery), " ","+");
 
     $.ajax({
         url: "https://api.spotify.com/v1/search?query="+strippedQuery+"&type=artist&offset=0&limit=20",
     }).done(function() {
         $( this ).addClass( "done" );
-    }).fail(function() {
-        alert( "error" );
+    }).fail(function(e) {
+        alert( "the following error has occured"+ e );
     });
 }
+
+function spotifyIFrame() {
+
+}
+/*
+<iframe src="https://embed.spotify.com/?uri=spotify%3Atrack%3A33Q6ldVXuJyQmqs8BmAa0k&theme=white" width="300" height="80" frameborder="0" allowtransparency="true"></iframe>
+ */
