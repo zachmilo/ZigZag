93c0533496cca3a6af7f8eb4f5d71d0b8f8c1637
zachmilo <zachmilo45@gmail.com>
2017-04-03 23:12:45 -0400

    merging my branch with dev

--- main/assets/javascript/shared.js
+++ main/assets/javascript/shared.js
@@ -1,18 +1,59 @@
 "use strict"
+/*
+* Allow you to search spotify api.
+* This search can do artist, albums, and track
+* returns: the search results
+ */
 
-function spotifySearch(searchQuery,artist, albums,track) {
-    var artistType = artist || "";
-    var albumType = albums  || "";
-    var trackType = track   || "";
 
-    // var strippedQuery = _.trim(searchQuery);
-    var strippedQuery = _.replace(_.trim(searchQuery), " ","+");
+function spotifySearch(searchQuery) {
+
+    var buildQuery = _.replace(_.trim(searchQuery), " ","+");
 
     $.ajax({
-        url: "https://api.spotify.com/v1/search?query="+strippedQuery+"&type=artist&offset=0&limit=20",
-    }).done(function() {
-        $( this ).addClass( "done" );
-    }).fail(function() {
-        alert( "error" );
+        url: "https://api.spotify.com/v1/search?query="+buildQuery+"&type=artist,track,album&offset=0&limit=20",
+    }).done(function(data) {
+        console.log(data);
+        return data;
+    }).fail(function(e) {
+        console.log( "An error occured trying with search query"+ e );
     });
 }
+
+function spotifyIFrame(albumId) {
+
+    var iSrc = "https://embed.spotify.com/?uri=spotify%3Aalbum%3A"+albumId+"&theme=white"
+    var frame = $("<iframe>");
+
+    $(frame).attr("src",iSrc);
+    $(frame).css({"width":"100%", "height":"80", "frameborder":"0", "allowtransparency":"true"})
+
+    //$("#test").append(frame);
+    return frame;
+    //.albums.items["0"].id
+}
+
+function getRelatedArtist(artistId) {
+    $.ajax({
+        url: "https://api.spotify.com/v1/artists/"+artistId+"/related-artists",
+    }).done(function(data) {
+        console.log(data);
+        return data;
+    }).fail(function(e) {
+        console.log( "Getting related artist failed"+ e );
+    });
+
+}
+/*
+<iframe src="https://embed.spotify.com/?uri=spotify%3Atrack%3A33Q6ldVXuJyQmqs8BmAa0k&theme=white" width="300" height="80" frameborder="0" allowtransparency="true"></iframe>
+ */
+/*
+
+#band picture
+.artists.items["0"].images[1].url
+
+#band name
+.artists.items["0"].name
+ */
+
+//GET https://api.spotify.com/v1/artists/{id}/related-artists
