b1787add41ca6cacfaa7aeae62d0a2158ffe1c58
zachmilo <zachmilo45@gmail.com>
2017-04-09 20:36:26 -0400

    added spotify testing

--- main/assets/javascript/shared.js
+++ main/assets/javascript/shared.js
@@ -1,50 +1,49 @@
 "use strict"
+
 /*
 * Allow you to search spotify api.
 * This search can do artist, albums, and track
 * returns: the search results
  */
-function spotifySearch(searchQuery) {
+function spotifySearch(searchQuery, callbackFunc) {
 
     var buildQuery = _.replace(_.trim(searchQuery), " ","+");
 
     $.ajax({
         url: "https://api.spotify.com/v1/search?query="+buildQuery+"&type=artist&offset=0&limit=20",
     }).done(function(data) {
-        console.log(data);
-        return data;
+        return callbackFunc(data);
     }).fail(function(e) {
         console.log( "An error occured trying with search query"+ e );
     });
 }
 
-function spotifyIFrame(artistId) {
-
+function spotifyIFrame(artistId, callbackFunc) {
+    var albumId = "";
     $.ajax({
         url: "https://api.spotify.com/v1/artists/"+artistId+"/albums",
     }).done(function(data) {
-        console.log(data);
-        return data;
+        var albumId = Math.floor((Math.random() * data.items.length));
+        var iSrc = "https://embed.spotify.com/?uri=spotify%3Aalbum%3A"+data.items[albumId].id+"&theme=white"
+        var frame = $("<iframe>");
+
+        $(frame).attr("src",iSrc);
+        $(frame).css({"width":"100%", "height":"80", "frameborder":"0", "allowtransparency":"true"})
+
+        return callbackFunc(frame);
+
     }).fail(function(e) {
         console.log( "An error occured trying to get album"+ e );
     });
-
-    var iSrc = "https://embed.spotify.com/?uri=spotify%3Aalbum%3A"+albumId+"&theme=white"
-    var frame = $("<iframe>");
-
-    $(frame).attr("src",iSrc);
-    $(frame).css({"width":"100%", "height":"80", "frameborder":"0", "allowtransparency":"true"})
-
-    return frame;
-
 }
 
-function getRelatedArtist(artistId) {
+function getRelatedArtist(artistId, callbackFunc) {
     $.ajax({
         url: "https://api.spotify.com/v1/artists/"+artistId+"/related-artists",
     }).done(function(data) {
         console.log(data);
-        return data;
+        return callbackFunc(data);
     }).fail(function(e) {
         console.log( "Getting related artist failed"+ e );
     });
+}
