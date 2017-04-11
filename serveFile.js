58377fcc30c7738c0cc08208c3baa546a469c522
zachmilo <zachmilo45@gmail.com>
2017-04-11 01:59:39 -0400

    heroku test

--- serveFile.js
+++ serveFile.js
@@ -2,9 +2,31 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
+app.set('port', (process.env.PORT || 5000));
 // viewed at http://localhost:8080
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.listen(8080);
+app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
+});
+//
+// var express = require('express');
+// var app = express();
+//
+// app.set('port', (process.env.PORT || 5000));
+//
+// app.use(express.static(__dirname + '/public'));
+//
+// // views is directory for all template files
+// app.set('views', __dirname + '/views');
+// app.set('view engine', 'ejs');
+//
+// app.get('/', function(request, response) {
+//   response.render('pages/index');
+// });
+//
+// app.listen(app.get('port'), function() {
+//   console.log('Node app is running on port', app.get('port'));
+// });
