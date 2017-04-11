7193e931de3228804ee8023d11bc2c1b02a1242e
zachmilo <zachmilo45@gmail.com>
2017-04-11 02:37:26 -0400

    working better now

--- index.js
+++ index.js
@@ -1,32 +1,33 @@
-var express = require('express');
-var app = express();
-var path = require('path');
-
-app.set('port', (process.env.PORT || 5000));
-// viewed at http://localhost:8080
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
-});
-
-app.listen(app.get('port'), function() {
-    console.log('Node app is running on port', app.get('port'));
-});
-//
 // var express = require('express');
 // var app = express();
+// var path = require('path');
 //
 // app.set('port', (process.env.PORT || 5000));
-//
-// app.use(express.static(__dirname + '/public'));
-//
-// // views is directory for all template files
-// app.set('views', __dirname + '/views');
-// app.set('view engine', 'ejs');
-//
-// app.get('/', function(request, response) {
-//   response.render('pages/index');
+// // viewed at http://localhost:8080
+// app.get('/', function(req, res) {
+//     res.sendFile(path.join(__dirname + 'index.html'));
 // });
 //
 // app.listen(app.get('port'), function() {
-//   console.log('Node app is running on port', app.get('port'));
+//     console.log('Node app is running on port', app.get('port'));
 // });
+
+
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname));
+
+// views is directory for all template files
+app.set('views', __dirname);
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.render('index')
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
