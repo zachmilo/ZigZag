15f8e4695002be05be0f789d1010ace86b80b12b
Phil Sautter <psautter@gmail.com>
2017-04-13 22:10:40 -0400

    fixed iframe overlap issue

--- main/assets/javascript/app.js
+++ main/assets/javascript/app.js
@@ -3,71 +3,73 @@
 $(() => {
     //Main Page
     $('#main_button').on('click', function(event) {
-        event.preventDefault();
-        quickValidate($('#band_name').val().trim(), $('#location').val().trim());
-    })
-
+            event.preventDefault();
+            quickValidate($('#band_name').val().trim(), $('#location').val().trim());
+        })
     //Results Page
-    $(".dropdown-button").dropdown();
-    //pulse effect for spotify activator playlist button
+    //Pulse effect for spotify activator playlist button
     $('body').on('mouseenter', '.card-action-buttons a', function() {
-        $(this).addClass('pulse');
-    }).on('mouseleave', '.card-action-buttons a', function() {
-        $(this).removeClass('pulse');
-    }).on('click', '.card-action-buttons a', function() {
-        $(this).removeClass('pulse');
-    })
-
+            $(this).addClass('pulse');
+        }).on('mouseleave', '.card-action-buttons a', function() {
+            $(this).removeClass('pulse');
+        }).on('click', '.card-action-buttons a', function() {
+            $(this).removeClass('pulse');
+        })
+    //Add or remove the pink class and the favorite property to the heart icon when clicked
     $('body').on('mouseenter', 'a.favorite', function() {
-        if (!$(this).prop('favorite')) {
-            $(this).addClass('pink')
-        }
-    }).on('mouseleave', 'a.favorite', function() {
-        if (!$(this).prop('favorite')) {
-            $(this).removeClass('pink')
-        }
-    }).on('click', 'a.favorite', function() {
-        if (!$(this).prop('favorite')) {
-            $(this).addClass('pink')
-                .prop('favorite', true);
-        } else {
-            $(this).removeClass('pink')
-                .prop('favorite', false)
-                .css('background-color', 'lightgray');
-        }
-    })
-
+            if (!$(this).prop('favorite')) {
+                $(this).addClass('pink')
+            }
+        }).on('mouseleave', 'a.favorite', function() {
+            if (!$(this).prop('favorite')) {
+                $(this).removeClass('pink')
+            }
+        }).on('click', 'a.favorite', function() {
+            if (!$(this).prop('favorite')) {
+                $(this).addClass('pink')
+                    .prop('favorite', true);
+            } else {
+                $(this).removeClass('pink')
+                    .prop('favorite', false)
+                    .css('background-color', 'lightgray');
+            }
+        })
     //Go! button on search modal
     $('#new-search').on('click', function(event) {
+            event.preventDefault();
+            quickValidate($('#band_name').val().trim(), $('#location').val().trim());
+        })
+    //Auth Modal click events
+    $('#sign-up').on('click', function(event) {
         event.preventDefault();
-        quickValidate($('#band_name').val().trim(), $('#location').val().trim());
-    })
-
-    //Auth Modal
-    $('#sign-up').on('click', function() {
         handleSignUp();
     })
-    $('#sign-in').on('click', function() {
+    $('#sign-in').on('click', function(event) {
+        event.preventDefault();
         toggleSignIn();
     })
-    $('#verify-email').on('click', function() {
+    $('#verify-email').on('click', function(event) {
+        event.preventDefault();
         sendEmailVerification();
     })
-    $('#password-reset').on('click', function() {
-        sendPasswordReset();
-    })
-
-
-    $('#new_band_name').val(sessionStorage.getItem('Hear+Now:bandName'));
-    $('#new_location').val(sessionStorage.getItem('Hear+Now:location'));
-
-    $('.modal').modal();
-
-    $('body').on('click', '.spotify', function() {
-        var _that = $(this);
-        spotifyIFrame($(this).attr('id'), function(frame) {
-            _that.closest('.card-image').next().next().append(frame);
+    $('#password-reset').on('click', function(event) {
+            event.preventDefault();
+            sendPasswordReset();
         })
+    //Populate the forms fields on the search modal with the previous input
+    $('#band_name').val(sessionStorage.getItem('Hear+Now:bandName'));
+    $('#location').val(sessionStorage.getItem('Hear+Now:location'));
+    //Enable the modal
+    $('.modal').modal();
+    //Populate the iframe when the volume_up icon is clicked
+    $('body').on('click', '.spotify', function() {
+        if ($(this).closest('.card-image').next().next().children('iframe').length === 0) {
+            //If there isn't already an iframe when clicked, create one
+            var _that = $(this);
+            spotifyIFrame($(this).attr('id'), function(frame) {
+                _that.closest('.card-image').next().next().append(frame);
+            })
+        }
     })
 })
 
@@ -76,11 +78,11 @@ function setSessionStorage(bandName, location) {
     sessionStorage.setItem('Hear+Now:location', location);
 }
 
-function loadNewResults() {
+function loadNewResults(id, images) {
     setSessionStorage($('#band_name').val().trim(), $('#location').val().trim());
     window.open("results.html", "_self");
 }
-
+//Validate the band name with Spotify and the zipcode with regex
 function quickValidate(bandName, location) {
     var reg = /^\d+$/;
 
@@ -96,7 +98,7 @@ function quickValidate(bandName, location) {
         url: "https://api.spotify.com/v1/search?query=" + bandName + "&type=artist,track,album&offset=0&limit=20",
     }).done(function(data) {
         if (data.artists.total != 0 && location) {
-            loadNewResults();
+            loadNewResults(data.artists.items[0].id, data.artists.items[0].images);
         } else if (data.artists.total === 0) {
             $('#band_name').val('');
             $('#band_name').attr('placeholder', 'PLEASE ENTER A VALID BAND NAME');
